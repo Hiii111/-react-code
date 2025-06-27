f1a00a313a7fcbaf0377e89af1260fed0b38cc31
Hiii111 <moshekhuansari@gmail.com>
2025-06-27 11:05:36 +0530

    cssModule

--- src/ExternalCss.jsx
+++ src/ExternalCss.jsx
@@ -1,9 +1,10 @@
-import './css/style.css';
+// import './css/style.css';
+import style from './css/userProfile.module.css';
 function ExternalCss(){
     return(
         <div>
-            <h1 className="heading">Welcome to India</h1>
-            <h2 className="secoundHeading">Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem, aliquam. Unde eaque iste maiores, vero repellendus praesentium dolores. Repellendus a harum nostrum quos ex rerum iure voluptatum consectetur doloremque ducimus!</h2>
+            <h1 className={style.heading}>Welcome Module CSS</h1>
+            <p className={style.content}>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Facilis quas sint porro officia, corporis voluptate veniam ipsa, tempore magni non, nostrum ad ipsam in modi neque dolorum hic dolorem iusto!</p>
         </div>
     )
 }
